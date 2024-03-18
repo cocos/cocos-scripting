05c9b2975ef9f4a6416fa16474e6115c1142a57d
James Chen <dumganhar@gmail.com>
2024-03-18 10:27:05 +0800

    Move entry code to entry directory.

--- entry/test/test-engine-source/@types/cc-ambient-types/src/index.d.ts
+++ entry/test/test-engine-source/@types/cc-ambient-types/src/index.d.ts
@@ -1,4 +1,4 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
-/// <reference path="../../../@types/consts.d.ts"/>
-/// <reference path="../../../@types/external-module.d.ts"/>
+/// <reference path="../../consts.d.ts"/>
+/// <reference path="../../external-module.d.ts"/>
 /// <reference path="../../../native/external/wasm/wasm-pack/wasm_rust.wasm.d.ts"/>
